96a9740254427a1af28815e32dcdc4359da26372
Vivek Kumar <vivekbxr30@gmail.com>
2025-08-05 19:16:27 +0530

    LECTURE-13 and LECTURE-14

---
+++ LECTURE-13/server.js
@@ -0,0 +1,61 @@
+const { json } = require("body-parser");
+const express=require("express");
+const fs=require("fs");
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname+"/public"));
+app.get("/", (req, res)=>{
+    res.send("Server Started");
+})
+app.get("/userData", (req, res)=>{
+    fs.readFile("./users.json", "utf-8", function(err, data){
+        if(err) res.send(err);
+        let allUsers=JSON.parse(data);
+        res.json(allUsers);
+    })
+})
+// app.post("/", (req, res)=>{
+//     let{id, name, username, role}=req.body;
+//     let allUsers=[];
+//     const newUser={
+//         id,
+//         name,
+//         username,
+//         role
+//     }
+//     fs.readFile("./users.json", "utf-8", function(err, data){
+//         if(err) res.send(err);
+//         let allUsers=JSON.parse(data);
+//         allUsers.push(newUser);
+
+//         fs.writeFile("./users.json", JSON.stringify(allUsers), function(err){
+//             if(err) res.send(err);
+//             res.send("User Registered Successfully");
+//         })
+//     })
+// })
+app.post("/addUser", (req, res) => {
+    try {
+        let { name, username } = req.body;
+        let newUser = {
+            id: Math.floor(Math.random() * 100000),
+            name: name,
+            username: username,
+            role: "user"
+        };
+        let allUser = [];
+        let data = fs.readFileSync("./users.json", "utf-8");
+        if (data) {
+            allUser = JSON.parse(data);
+        }
+        allUser.push(newUser);
+        fs.writeFileSync("./users.json", JSON.stringify(allUser));
+        res.json(newUser);
+    } catch (error) {
+        return res.status(500).send("Error: " + error.message);
+    }
+});
+app.listen(3333, ()=>{
+    console.log("Server Started");
+})
