ae795743dfad6768d29ffd1ccbd0b290e0d9c73b
Vivek Kumar <vivekbxr30@gmail.com>
2025-10-13 15:47:16 +0530

    LECTURE-34

---
+++ LECTURE-34/index.js
@@ -0,0 +1,32 @@
+const express=require('express');
+const {createClient}=require('redis');
+const app=express();
+
+const client =createClient({url: 'redis://localhost:6379'});
+client.on('error', (err) => console.error(' Redis Client Error', err));
+
+(async () => {
+  await client.connect();
+  console.log('Connected to Redis');
+})();
+
+async function cacheUserProfile() {
+    await client.set("user:1", JSON.stringify({name:"Vivek Kumar", age:"20"}));
+    
+}
+async function readProfile() {
+    let data=await client.get("user:1");
+   retu
+}
+
+// cacheUserProfile()
+// .then(()=>{
+//     console.log("Profile cached");
+// });
+
+readProfile()
+.then
+
+app.listen(3333, ()=>{
+    console.log("server started");
+})
